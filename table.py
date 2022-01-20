b62872b93a757b4b7a199a100b9ebf04b1bfb532
ushabisht21 <93266927+ushabisht21@users.noreply.github.com>
2022-01-20 11:42:01 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,13 @@
+# list=[1,2,3,4,5]
+# new_list=[]
+# i=1
+# while i<=len(list):
+#     j=1
+#     while j<=10:
+#         t=j*i
+#         new_list.append(t)
+#         j=j+1
+#     i=i+1
+# print(new_list)
+
+
