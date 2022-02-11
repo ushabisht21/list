30ba6ff4a562bcf52a42d240b5abf1f0fb872850
ushabisht21 <93266927+ushabisht21@users.noreply.github.com>
2022-02-11 12:20:22 +0530

    Add files via upload

--- zero remove.py
+++ zero remove.py
@@ -1,8 +1,17 @@
-a=[2,0,3,0,4,0,4,0,5,0]
+a=[101,101,1011,110]
+b=[]
 i=0
-k=[]
-while i<len(a):
-    if a[i]!=0:
-        k.append(a[i])
+while i< len(a):
+    h=str(a[i])
+    s=""
+    j=0
+    while j<len(h):
+        if h[j] in "0":
+            pass
+        else:
+            s+=h[j]
+        j=j+1
+    b.append(int(s))
     i=i+1
-print(k)
+print(b)
+
